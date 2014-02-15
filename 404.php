613383921319f97ffc6d5e8fa5a47c8e07537bd4
Cumhur Korkut <cumhurkorkut@gmail.com>
2014-02-15 10:54:28 -0500

    404 & search & tag pages design

--- 404.php
+++ 404.php
@@ -10,7 +10,34 @@
  */
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
-
-<h2>Page not found</h2>
+      <div class="row">
+          <div class="row content article">
+            <div class="col-md-9">
+              <div class="relative">
+                  <div class="article-text">
+					<h2>Page not found</h2>
+				  </div>
+				</div>
+            </div>
+            <div class="col-md-3">
+              <div class="box">
+                <h2><a href="section.html">Media</a></h2>
+                <ul>
+                  <li><a href="article.html"><strong>REALLY COOL TITLE</strong>By Lily, EIC</a></li>
+                  <li><a href="article.html"><strong>REALLY COOL TITLE</strong>By Christina, EIC</a></li>
+                  <li><a href="article.html"><strong>REALLY COOL TITLE</strong>By Miranda, EIC</a></li>
+                </ul>
+              </div>
+              <div class="box">
+                <h2><a href="section.html">Popular</a></h2>
+                <ul>
+                  <li><a href="article.html"><strong>REALLY COOL TITLE</strong>By Lily, EIC</a></li>
+                  <li><a href="article.html"><strong>REALLY COOL TITLE</strong>By Christina, EIC</a></li>
+                  <li><a href="article.html"><strong>REALLY COOL TITLE</strong>By Miranda, EIC</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+      </div>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
--- tag.php
+++ tag.php
@@ -10,22 +10,67 @@
  */
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
-
+      <div class="row">
+          <div class="row content article">
+            <div class="col-md-9">
+              <div class="relative">
+              
+                <div class="article-text">
 <?php if ( have_posts() ): ?>
-<h2>Tag Archive: <?php echo single_tag_title( '', false ); ?></h2>
-<ol>
+
+                
+                <button type="button" class="btn btn-argus pull-right">Newer posts &raquo;</button>
+                <button type="button" class="btn btn-argus pull-right">&laquo; Older posts</button>
+
+                <h1><span>Tag Archive: <?php echo single_tag_title( '', false ); ?><span></h1>
+                <div class="clearfix"></div>
+                </div>
+              </div>
+             
+              <div class="article-text">
+
 <?php while ( have_posts() ) : the_post(); ?>
-	<li>
-		<article>
-			<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
-			<?php the_content(); ?>
-		</article>
-	</li>
+		<section>
+          <h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+          <h4><time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?></h4>
+          <p><?php the_excerpt(); ?></p>
+        </section>
+
 <?php endwhile; ?>
-</ol>
+              </div>
+              <div class="article-text">
+              <button type="button" class="btn btn-argus pull-right">Newer posts &raquo;</button>
+                <button type="button" class="btn btn-argus pull-right">&laquo; Older posts</button>
+                <div class="clearfix"></div>
+              </div>
 <?php else: ?>
-<h2>No posts to display in <?php echo single_tag_title( '', false ); ?></h2>
+
+					<h2>No posts to display in <?php echo single_tag_title( '', false ); ?></h2>
+				</div>
+			</div>
+
 <?php endif; ?>
+
+            </div>
+            <div class="col-md-3">
+              <div class="box">
+                <h2><a href="section.html">Media</a></h2>
+                <ul>
+                  <li><a href="article.html"><strong>REALLY COOL TITLE</strong>By Lily, EIC</a></li>
+                  <li><a href="article.html"><strong>REALLY COOL TITLE</strong>By Christina, EIC</a></li>
+                  <li><a href="article.html"><strong>REALLY COOL TITLE</strong>By Miranda, EIC</a></li>
+                </ul>
+              </div>
+              <div class="box">
+                <h2><a href="section.html">Popular</a></h2>
+                <ul>
+                  <li><a href="article.html"><strong>REALLY COOL TITLE</strong>By Lily, EIC</a></li>
+                  <li><a href="article.html"><strong>REALLY COOL TITLE</strong>By Christina, EIC</a></li>
+                  <li><a href="article.html"><strong>REALLY COOL TITLE</strong>By Miranda, EIC</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+      </div>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
