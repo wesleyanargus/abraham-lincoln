f237fca1c13065bedb71d4717b1b0356b91eb781
Cumhur Korkut <cumhurkorkut@gmail.com>
2014-03-01 19:58:00 -0500

    author page

--- author.php
+++ author.php
@@ -10,31 +10,50 @@
  */
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+      <div class="row">
+          <div class="row content article">
+            <div class="col-md-9">
+              <div class="relative">
+              
+                <div class="article-text">
+<?php if ( have_posts() ): ?>
+                <button type="button" class="btn btn-argus pull-right">Newer posts &raquo;</button>
+                <button type="button" class="btn btn-argus pull-right">&laquo; Older posts</button>
 
-<?php if ( have_posts() ): the_post(); ?>
+                <?php if ( get_the_author_meta( 'description' ) ) : ?>
+				<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
+				<h3>About <?php echo get_the_author() ; ?></h3>
+				<?php the_author_meta( 'description' ); ?>
+				<?php endif; ?>
+                <div class="clearfix"></div>
+                </div>
+              </div>
+             
+              <div class="article-text">
 
-<h2>Author Archives: <?php echo get_the_author() ; ?></h2>
-
-<?php if ( get_the_author_meta( 'description' ) ) : ?>
-<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
-<h3>About <?php echo get_the_author() ; ?></h3>
-<?php the_author_meta( 'description' ); ?>
-<?php endif; ?>
-
-<ol>
 <?php rewind_posts(); while ( have_posts() ) : the_post(); ?>
-	<li>
-		<article>
-			<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
-			<?php the_content(); ?>
-		</article>
-	</li>
+	<section>
+        <h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+        <h4><time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?></h4>
+        <p><?php the_excerpt(); ?></p>
+    </section>
 <?php endwhile; ?>
-</ol>
-
+              </div>
+              <div class="article-text">
+              <button type="button" class="btn btn-argus pull-right">Newer posts &raquo;</button>
+                <button type="button" class="btn btn-argus pull-right">&laquo; Older posts</button>
+                <div class="clearfix"></div>
+              </div>
 <?php else: ?>
-<h2>No posts to display for <?php echo get_the_author() ; ?></h2>	
+					<h2>No posts to display for <?php echo get_the_author() ; ?></h2>	
+				</div>
+			</div>
 <?php endif; ?>
+            </div>
+            <div class="col-md-3">
+            <?php get_sidebar(); ?>
+            </div>
+          </div>
+      </div>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
